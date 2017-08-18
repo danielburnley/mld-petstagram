b934277b1b317bcc31dbee3bd9a075a9d930040e
Daniel Burnley <dan@danops.uk>
2017-08-18 12:24:41 +0100

    Remove old files

--- components/MadeLogo/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import MadeLogo from './'
-
-storiesOf('MadeLogo', module)
-  .add('default', () =>
-    <MadeLogo />
-  )
--- pages/HomePage/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import HomePage from './'
-
-storiesOf('HomePage', module)
-  .add('default', () =>
-    <HomePage />
-  )
